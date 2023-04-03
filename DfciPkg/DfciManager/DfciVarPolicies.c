1665d85c8190afc174d4049a3ceb192db3e311b3
Sean Brogan <spbrogan@live.com>
2023-04-03 16:08:19 -0700

    Revert VarPolicy locking change

--- DfciPkg/DfciManager/DfciVarPolicies.c
+++ DfciPkg/DfciManager/DfciVarPolicies.c
@@ -13,53 +13,6 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 
 #include <DfciVariablePolicies.h>
 
-STATIC EDKII_VARIABLE_POLICY_PROTOCOL  *mVariablePolicy = NULL;
-
-/**
- * Event callback for Ready To Boot.
- * This is needed to lock certain variables
- *
- * @param Event
- * @param Context
- *
- * @return VOID EFIAPI
- */
-VOID
-EFIAPI
-ReadyToBootCallback (
-  IN EFI_EVENT  Event,
-  IN VOID       *Context
-  )
-{
-  UINT8       Lock;
-  EFI_STATUS  Status;
-
-  gBS->CloseEvent (Event);
-
-  if (mVariablePolicy == NULL) {
-    ASSERT (mVariablePolicy != NULL);
-    return;
-  }
-
-  //
-  // Lock most variables at ReadyToBoot
-  //
-  Lock   = 1;
-  Status = gRT->SetVariable (
-                  DFCI_LOCK_VAR_NAME,
-                  &gDfciLockVariableGuid,
-                  DFCI_LOCK_VAR_ATTRIBUTES,
-                  sizeof (Lock),
-                  &Lock
-                  );
-
-  if (EFI_ERROR (Status)) {
-    DEBUG ((DEBUG_ERROR, "%a %a: Set DfciLock at ReadyToBoot failed. Code=%r!\n", _DBGMSGID_, __FUNCTION__, Status));
-  } else {
-    DEBUG ((DEBUG_INFO, "%a %a: - Dfci Lock variable(%s) set to 1, and locked\n", _DBGMSGID_, __FUNCTION__, DFCI_LOCK_VAR_NAME));
-  }
-}
-
 /**
   InitializeAndSetPolicyForAllDfciVariables
 
@@ -69,51 +22,31 @@ InitializeAndSetPolicyForAllDfciVariables (
   VOID
   )
 {
-  UINTN       i;
-  EFI_STATUS  Status;
-  EFI_EVENT   Event;
+  UINTN                           i;
+  EFI_STATUS                      Status;
+  EDKII_VARIABLE_POLICY_PROTOCOL  *VariablePolicy = NULL;
 
-  //
-  // Request notification of ReadyToBoot.
-  //
-  // NOTE: If this fails, the variables are not locked
-  //       DELAYED_PROCESSING state.
-  //
-  Status = gBS->CreateEventEx (
-                  EVT_NOTIFY_SIGNAL,
-                  TPL_CALLBACK - 1,  // Lock the Dfci Variables after all other ReadyToBoot handlers
-                  ReadyToBootCallback,
-                  NULL,
-                  &gEfiEventReadyToBootGuid,
-                  &Event
-                  );
-
-  if (EFI_ERROR (Status)) {
-    DEBUG ((DEBUG_ERROR, "%a %a: ReadyToBoot callback registration failed! %r\n", _DBGMSGID_, __FUNCTION__, Status));
-    // Continue with rest of variable locks if possible.
-  }
-
-  Status = gBS->LocateProtocol (&gEdkiiVariablePolicyProtocolGuid, NULL, (VOID **)&mVariablePolicy);
+  Status = gBS->LocateProtocol (&gEdkiiVariablePolicyProtocolGuid, NULL, (VOID **)&VariablePolicy);
   if (EFI_ERROR (Status)) {
     DEBUG ((DEBUG_ERROR, "%a %a: - Locating Variable Policy failed - Code=%r\n", _DBGMSGID_, __FUNCTION__, Status));
     goto Done;
   }
 
   //
-  // Lock most variables when DfciLock variable is created.
+  // Lock most variables when ReadyToBoot Phase variable is set
   //
   for (i = 0; i < ARRAY_SIZE (gReadyToBootPolicies); i++) {
     Status = RegisterVarStateVariablePolicy (
-               mVariablePolicy,
+               VariablePolicy,
                gReadyToBootPolicies[i].Namespace,
                gReadyToBootPolicies[i].Name,
                gReadyToBootPolicies[i].MinSize,
                gReadyToBootPolicies[i].MaxSize,
                gReadyToBootPolicies[i].AttributesMustHave,
                gReadyToBootPolicies[i].AttributesCantHave,
-               &gDfciLockVariableGuid,
-               DFCI_LOCK_VAR_NAME,
-               1
+               &gMuVarPolicyDxePhaseGuid,
+               READY_TO_BOOT_INDICATOR_VAR_NAME,
+               PHASE_INDICATOR_SET
                );
 
     if (EFI_ERROR (Status)) {
@@ -123,46 +56,11 @@ InitializeAndSetPolicyForAllDfciVariables (
   }
 
   //
-  // Make sure there is no existing lock variable
-  //
-  Status = gRT->SetVariable (
-                  DFCI_LOCK_VAR_NAME,
-                  &gDfciLockVariableGuid,
-                  0,
-                  0,
-                  NULL
-                  );
-
-  if (Status == EFI_SUCCESS) {
-    DEBUG ((DEBUG_ERROR, "%a %a: Existing variable found, and deleted  This should not happen.\n", _DBGMSGID_, __FUNCTION__));
-  } else if (Status != EFI_NOT_FOUND) {
-    DEBUG ((DEBUG_ERROR, "%a %a: Error deleting Dfci Lock variable(%s). Code=%r\n", _DBGMSGID_, __FUNCTION__, DFCI_LOCK_VAR_NAME, Status));
-  }
-
-  for (i = 0; i < ARRAY_SIZE (gDfciLockPolicy); i++) {
-    Status = RegisterBasicVariablePolicy (
-               mVariablePolicy,
-               gDfciLockPolicy[i].Namespace,
-               gDfciLockPolicy[i].Name,
-               gDfciLockPolicy[i].MinSize,
-               gDfciLockPolicy[i].MaxSize,
-               gDfciLockPolicy[i].AttributesMustHave,
-               gDfciLockPolicy[i].AttributesCantHave,
-               VARIABLE_POLICY_TYPE_LOCK_ON_CREATE
-               );
-
-    if (EFI_ERROR (Status)) {
-      DEBUG ((DEBUG_ERROR, "%a %a: - RegisterBasicVariablePolicy() DfciLock[%d] returned %r!\n", _DBGMSGID_, __FUNCTION__, i, Status));
-      DEBUG ((DEBUG_ERROR, "%a %a: - Error registering %g:%s\n", _DBGMSGID_, __FUNCTION__, gDfciLockPolicy[i].Namespace, gDfciLockPolicy[i].Name));
-    }
-  }
-
-  //
   // The mailboxes are not locked, but set restrictions for variable sizes and attributes
   //
   for (i = 0; i < ARRAY_SIZE (gMailBoxPolicies); i++) {
     Status = RegisterBasicVariablePolicy (
-               mVariablePolicy,
+               VariablePolicy,
                gMailBoxPolicies[i].Namespace,
                gMailBoxPolicies[i].Name,
                gMailBoxPolicies[i].MinSize,
