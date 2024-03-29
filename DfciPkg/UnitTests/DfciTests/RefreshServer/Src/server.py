45e3d93e917d73b0a6db145d524f48b46e427f67
Doug Flick <dougflick@microsoft.com>
2024-03-28 22:32:11 -0700

    Control TLS Cipher Suite

--- DfciPkg/UnitTests/DfciTests/RefreshServer/Src/server.py
+++ DfciPkg/UnitTests/DfciTests/RefreshServer/Src/server.py
@@ -4,9 +4,9 @@
 import cherrypy
 from main import dfci_refresh_server
 
+from OpenSSL import SSL
 
 if __name__ == '__main__':
-
     # Mount the application
     cherrypy.tree.graft(dfci_refresh_server, "/")
 
@@ -14,7 +14,10 @@ if __name__ == '__main__':
     cherrypy.server.unsubscribe()
     cherrypy.engine.signals.subscribe()
 
-    cherrypy.config.update({'environment': 'embedded'})
+    cherrypy.config.update({
+        'environment': 'embedded',
+        'log.screen': True,
+        })
 
     # Instantiate a new server object
     server_http = cherrypy._cpserver.Server()
@@ -25,21 +28,38 @@ if __name__ == '__main__':
     server_http.thread_pool = 30
 
     server_https = cherrypy._cpserver.Server()
+    server_https.ssl_module = 'PyOpenSSL'
 
-    # Configure the server object
-    server_https.ssl_module = 'builtin'
+    ctx = SSL.Context(SSL.TLS_SERVER_METHOD)
+    ctx.use_privatekey_file('ssl/DFCI_HTTPS.key')
+    ctx.use_certificate_file('ssl/DFCI_HTTPS.pem')
+
+    # Set the Maximum Protocol Version supported by Intune
+    ctx.set_max_proto_version(SSL.TLS1_2_VERSION)
+
+    # Set the Minimum Protocol Version supported by Intune
+    ctx.set_min_proto_version(SSL.TLS1_2_VERSION)
+
+    # Set the Cipher List supported by Intune
+    # You may verify the cipher list with the following command:
+    # nmap --script ssl-enum-ciphers -p 443 127.0.0.1
+    
+    cipher_list = [b'TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384',
+                   b'TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256',
+                   b'TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384', 
+                   b'TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256']
+    
+    ctx.set_cipher_list(b':'.join(cipher_list))
+    
+    server_https.ssl_context = ctx
     server_https.socket_host = "0.0.0.0"
     server_https.socket_port = 443
     server_https.thread_pool = 30
-    server_https.ssl_certificate = 'ssl/DFCI_HTTPS.pem'
-    server_https.ssl_private_key = 'ssl/DFCI_HTTPS.key'
 
     # Subscribe this server
     server_http.subscribe()
     server_https.subscribe()
 
     # Start the server engine (Option 1 *and* 2)
-
     cherrypy.engine.start()
-
     cherrypy.engine.block()
